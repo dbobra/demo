e23d98ab00b331dea18ad1e4efec0f51a3958f17
dbobra <bobra.darshan@gmail.com>
2015-01-15 12:44:28 +0530

    Add outpur statement.

--- Demo-project/src/com/demo/sample/MainClass.java
+++ Demo-project/src/com/demo/sample/MainClass.java
@@ -3,5 +3,6 @@ package com.demo.sample;
 public class MainClass {
 public static void main(String[] args) {
 	System.out.println("Test the github account");
+	System.out.println("Changes in file");
 }
 }
