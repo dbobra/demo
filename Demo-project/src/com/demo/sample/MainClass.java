699bd2301ae1862866fa181ea9ec7bee3b97cfc8
dbobra <bobra.darshan@gmail.com>
2015-01-15 13:08:41 +0530

    Thrid commit ..

--- Demo-project/src/com/demo/sample/MainClass.java
+++ Demo-project/src/com/demo/sample/MainClass.java
@@ -4,5 +4,6 @@ public class MainClass {
 public static void main(String[] args) {
 	System.out.println("Test the github account");
 	System.out.println("Changes in file");
+	System.out.println("Learning the logs of git ");
 }
 }
